7a2cb23cc169c46aa8ee67c7784628143703a2b4
Genius <149167886+Salvatore-droid@users.noreply.github.com>
2025-03-22 13:10:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ signupButton.addEventListener("click", signup)
 loginButton.addEventListener("click", login)
 closeButton.addEventListener("click", () => modal.close())
 
-const SERVER_URL = "http://localhost:3000"
+const SERVER_URL = "https://figerprint-auther-backend.onrender.com/"
 
 async function signup() {
   const email = emailInput.value
